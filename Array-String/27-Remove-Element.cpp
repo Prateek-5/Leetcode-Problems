5940da133228a1da10b25560b7d84a8e87d3220b
Prateek-5 <prateek.ku11@gmail.com>
2024-02-09 14:20:16 +0530

    added the logic for the for problem 27

---
+++ Array-String/27-Remove-Element.cpp
@@ -0,0 +1,84 @@
+/*
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.
+
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+Custom Judge:
+
+The judge will test your solution with the following code:
+
+int[] nums = [...]; // Input array
+int val = ...; // Value to remove
+int[] expectedNums = [...]; // The expected answer with correct length.
+                            // It is sorted with no values equaling val.
+
+int k = removeElement(nums, val); // Calls your implementation
+
+assert k == expectedNums.length;
+sort(nums, 0, k); // Sort the first k elements of nums
+for (int i = 0; i < actualLength; i++) {
+    assert nums[i] == expectedNums[i];
+}
+If all assertions pass, then your solution will be accepted.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+
+Input: nums = [0,1,2,2,3,0,4,2], val = 2
+Output: 5, nums = [0,1,4,0,3,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums containing 0, 0, 1, 3, and 4.
+Note that the five elements can be returned in any order.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+public:
+    
+    int removeElement(vector<int>& nums, int val) {
+        int count=0;
+        /*
+            The count variable keeps the track of the elements which are not equal to 
+            the the val give 
+            Note all the elements to the left of the count variable are throus which are
+            not equal to the given value
+
+            As soon as we find any element which is not equal ot he given value we swap it with the 
+            nums[i],nums[count]
+
+            return the count valiable it gives the count of all elemets whichare not equal to the val
+            
+
+        */
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=val){
+            swap(nums[i],nums[count]);
+            count++;
+            }
+        }
+        return count;
+
+    }
+};
