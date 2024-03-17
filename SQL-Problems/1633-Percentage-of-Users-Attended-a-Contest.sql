dddacc05df4655fec5eefac4f1580cc99035b5c0
Prateek-5 <prateek.ku11@gmail.com>
2024-03-17 21:15:01 +0530

    Added the solution for 1633

---
+++ SQL-Problems/1633-Percentage-of-Users-Attended-a-Contest.sql
@@ -0,0 +1,115 @@
+/*
+Table: Users
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| user_id     | int     |
+| user_name   | varchar |
++-------------+---------+
+user_id is the primary key (column with unique values) for this table.
+Each row of this table contains the name and the id of a user.
+ 
+
+Table: Register
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| contest_id  | int     |
+| user_id     | int     |
++-------------+---------+
+(contest_id, user_id) is the primary key (combination of columns with unique values) for this table.
+Each row of this table contains the id of a user and the contest they registered into.
+ 
+
+Write a solution to find the percentage of the users registered in each contest rounded to two decimals.
+
+Return the result table ordered by percentage in descending order. In case of a tie, order it by contest_id in ascending order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Users table:
++---------+-----------+
+| user_id | user_name |
++---------+-----------+
+| 6       | Alice     |
+| 2       | Bob       |
+| 7       | Alex      |
++---------+-----------+
+Register table:
++------------+---------+
+| contest_id | user_id |
++------------+---------+
+| 215        | 6       |
+| 209        | 2       |
+| 208        | 2       |
+| 210        | 6       |
+| 208        | 6       |
+| 209        | 7       |
+| 209        | 6       |
+| 215        | 7       |
+| 208        | 7       |
+| 210        | 2       |
+| 207        | 2       |
+| 210        | 7       |
++------------+---------+
+Output: 
++------------+------------+
+| contest_id | percentage |
++------------+------------+
+| 208        | 100.0      |
+| 209        | 100.0      |
+| 210        | 100.0      |
+| 215        | 66.67      |
+| 207        | 33.33      |
++------------+------------+
+Explanation: 
+All the users registered in contests 208, 209, and 210. The percentage is 100% and we sort them in the answer table by contest_id in ascending order.
+Alice and Alex registered in contest 215 and the percentage is ((2/3) * 100) = 66.67%
+Bob registered in contest 207 and the percentage is ((1/3) * 100) = 33.33% 
+
+
+*/
+
+# Write your MySQL query statement below
+
+/*
+    Intution / Basic Algorithm
+
+    Basically in the register table we need to group by the contest id and then keep the count og the user_id
+        throught this we will get the participation in each individual contest
+    Once we ahve the [articipation in each individual contest we will divide it by the totol participation and
+    retunr the result in order of precentage caluclated above and contest_id
+*/
+
+
+
+WITH CTE1 AS
+(
+    SELECT 
+        contest_id,
+        COUNT(user_id) AS participents
+    FROM Register
+    GROUP BY contest_id
+),
+CTE2 AS
+(
+    SELECT COUNT(*) AS total_participents
+    FROM Users
+)
+
+
+
+SELECT 
+    c.contest_id,
+    ROUND((participents/total_participents)*100,2) AS percentage
+    FROM CTE1 c,CTE2 b
+    ORDER BY 
+    percentage DESC,
+    c.contest_id;
